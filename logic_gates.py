8a600962b03133c8e141c5e97a8bb371fe7ab22d
Nabil Khalil <89206827+nkhalil98@users.noreply.github.com>
2022-12-10 20:20:15 -0500

    Update logic_gates.py

--- logic_gates.py
+++ logic_gates.py
@@ -19,7 +19,7 @@ print("NOT Gate Truth Table:\n")
 print("A: 0 | Output: {0}".format(NOT_gate(0)))
 print("A: 1 | Output: {0}\n".format(NOT_gate(1)))
 
-def AND_gate(a,b):
+def AND_gate(a, b):
   return int(a and b)
 
 # Truth Table
@@ -29,7 +29,7 @@ print("A: 0, B: 1 | Output: {0}".format(AND_gate(0, 1)))
 print("A: 1, B: 0 | Output: {0}".format(AND_gate(1, 0)))
 print("A: 1, B: 1 | Output: {0}\n".format(AND_gate(1, 1)))
 
-def OR_gate(a,b):
+def OR_gate(a, b):
   return int(a or b)
 
 # Truth Table
@@ -39,7 +39,7 @@ print("A: 0, B: 1 | Output: {0}".format(OR_gate(0, 1)))
 print("A: 1, B: 0 | Output: {0}".format(OR_gate(1, 0)))
 print("A: 1, B: 1 | Output: {0}\n".format(OR_gate(1, 1)))
 
-def XOR_gate(a,b):
+def XOR_gate(a, b):
   return int(a and not b) or int(not a and b)
 
 # Truth Table
