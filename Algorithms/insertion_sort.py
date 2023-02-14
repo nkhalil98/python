96eb791baf26fb29322d461b8a58240e7fc26463
Nabil Khalil <89206827+nkhalil98@users.noreply.github.com>
2023-02-14 13:27:50 -0500

    Add files via upload

---
+++ Algorithms/insertion_sort.py
@@ -0,0 +1,13 @@
+# insertion sort has a worst-case time complexity of O(n^2)
+# insertion sort has a space complexity of O(1)
+# insertion sort has a best-case time complexity of Ω(n)
+
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        anchor = arr[i]
+        j = i - 1
+        while j >=0 and anchor < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = anchor
