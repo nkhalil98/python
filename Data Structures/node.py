cfed2f4ed4d7d80a768f5e4dc80f42f6f5b3bb93
Nabil Khalil <89206827+nkhalil98@users.noreply.github.com>
2023-04-12 12:08:29 -0400

    Update node.py

--- Data Structures/node.py
+++ Data Structures/node.py
@@ -1,14 +1,14 @@
 # nodes are the fundamental building blocks of linked lists, stacks, queues, trees, and more
 
 class Node():
-    def __init__(self, val, next=None):
+    def __init__(self, val=None, next=None):
         self.val = val
         self.next = next
         
-    def get_next_node(self):
+    def get_next(self):
         return self.next
 
-    def set_next_node(self, node):
+    def set_next(self, node):
         self.next = node
         
     def get_val(self):
