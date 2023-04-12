d09559b159dd859eb21bdc147b8f84c056386b1e
Nabil Khalil <89206827+nkhalil98@users.noreply.github.com>
2023-04-12 11:58:17 -0400

    Update node.py

--- Data Structures/node.py
+++ Data Structures/node.py
@@ -2,12 +2,15 @@ class Node():
     def __init__(self, val, next=None):
         self.val = val
         self.next = next
+        
+    def get_next_node(self):
+        return self.next
 
     def set_next_node(self, node):
         self.next = node
-    
-    def get_next_node(self):
-        return self.next
         
     def get_val(self):
         return self.val
+    
+    def set_val(self, new_val):
+        self.val = new_val
