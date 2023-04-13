ba0776d87b4a44d0644718120790baa60c1a6763
Nabil Khalil <89206827+nkhalil98@users.noreply.github.com>
2023-04-13 17:03:35 -0400

    Add files via upload

---
+++ Algorithms/pattern_search.py
@@ -0,0 +1,10 @@
+def pattern_search(text, pattern):
+    for i in range(len(text)):
+        match_count = 0
+        for j in range(len(pattern)):
+            if pattern[j] == text[i+j]:
+                match_count += 1
+            else:
+                break
+        if match_count == len(pattern):
+            print(f'{pattern} found at index {i}')
