1beb8eac816c48e6a66fca05070e4ab0e4214863
nkhalil98 <nabilamirkhalil@proton.me>
2025-03-05 01:04:26 -0800

    bst

--- ds/bst.py
+++ ds/bst.py
@@ -1,24 +1,17 @@
-# a binary tree is a tree where every node has at most 2 children
-# a strict/proper binary tree is a binary tree where each node can have either 0 or 2 children
-# a complete binary tree is a binary tree where all levels possibly the last are completely filled and all nodes are as left as possible
-# a perfect binary tree is a binary tree where all levels are filled
-# a binary tree can have a maximum of 2^i nodes at level i
-# maximum number of nodes in a binary tree of height h is 2^(h+1) - 1
-# min height of a binary tree with n nodes is in a perfect binary tree which is log2(n)
-# max height of a binary tree with n nodes is in a sparse binary tree which is n-1
-# a balanced binary tree is a binary tree where the difference between the height of the left sub-tree and the right sub-tree for every node is not more than k (usually 1)
-# in binary search tree any node is smaller than a node to its right and larger than a node to its left
-# binary search trees cannot contain duplicate elements
-# searching for an element in a BST is O(log(n))
-# a BST can be traversed using in-order traversal (left sub-tree -> root -> right sub-tree), pre-order traversal (root -> left sub-tree -> right sub-tree), or post-order traversal (left sub-tree -> right sub-tree -> root)
+"""
+Binary Search Tree (BST)
+"""
+
+from __future__ import annotations
+
 
 class BinarySearchTreeNode:
-    def __init__(self, val):
+    def __init__(self, val):  # O(1)
         self.val = val
         self.left = None
         self.right = None
 
-    def add_child(self, val):
+    def add_child(self, val):  # O(log n) average, O(n) worst
         if val == self.val:
             return
 
@@ -33,8 +26,7 @@ class BinarySearchTreeNode:
             else:
                 self.right = BinarySearchTreeNode(val)
 
-
-    def search(self, val):
+    def search(self, val):  # O(log n) average, O(n) worst
         if self.val == val:
             return True
 
@@ -50,7 +42,7 @@ class BinarySearchTreeNode:
             else:
                 return False
 
-    def in_order_traversal(self):
+    def in_order_traversal(self):  # O(n)
         elements = []
 
         if self.left:
@@ -62,8 +54,8 @@ class BinarySearchTreeNode:
             elements += self.right.in_order_traversal()
 
         return elements
-    
-    def post_order_traversal(self):
+
+    def post_order_traversal(self):  # O(n)
         elements = []
 
         if self.left:
@@ -76,7 +68,7 @@ class BinarySearchTreeNode:
 
         return elements
 
-    def pre_order_traversal(self):
+    def pre_order_traversal(self):  # O(n)
         elements = [self.val]
 
         if self.left:
@@ -87,7 +79,7 @@ class BinarySearchTreeNode:
 
         return elements
 
-    def delete(self, val):
+    def delete(self, val):  # O(log n) average, O(n) worst
         if val < self.val:
             if self.left:
                 self.left = self.left.delete(val)
@@ -108,7 +100,7 @@ class BinarySearchTreeNode:
 
         return self
 
-    def delete2(self, val):
+    def delete2(self, val):  # O(log n) average, O(n) worst
         if val < self.data:
             if self.left:
                 self.left = self.left.delete(val)
@@ -129,17 +121,17 @@ class BinarySearchTreeNode:
 
         return self
 
-    def find_max(self):
+    def find_max(self):  # O(log n) average, O(n) worst
         if self.right is None:
             return self.val
         return self.right.find_max()
 
-    def find_min(self):
+    def find_min(self):  # O(log n) average, O(n) worst
         if self.left is None:
             return self.val
         return self.left.find_min()
-    
-    def calculate_sum(self):
+
+    def calculate_sum(self):  # O(n)
         left_sum = self.left.calculate_sum() if self.left else 0
         right_sum = self.right.calculate_sum() if self.right else 0
         return self.val + left_sum + right_sum
