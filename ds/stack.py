0b4bc44b78ce6ab006c388d6c546f6c2f3b61b1d
nkhalil98 <nabilamirkhalil@proton.me>
2025-03-04 22:41:39 -0800

    add O(n) complexities

--- ds/stack.py
+++ ds/stack.py
@@ -9,55 +9,55 @@ from collections import deque
 
 # stack implementation using deque
 class Stack:
-    def __init__(self):
+    def __init__(self):  # O(1)
         self.stk = deque()
 
-    def push(self, val):
+    def push(self, val):  # O(1)
         self.stk.append(val)
 
-    def pop(self):
+    def pop(self):  # O(1)
         if self.is_empty():
             return None
         return self.stk.pop()
 
-    def peek(self):
+    def peek(self):  # O(1)
         if self.is_empty():
             return None
         return self.stk[-1]
 
-    def clear(self):
+    def clear(self):  # O(1)
         self.stk = deque()
 
-    def is_empty(self):
+    def is_empty(self):  # O(1)
         return len(self.stk) == 0
 
-    def size(self):
+    def size(self):  # O(1)
         return len(self.stk)
 
 
 # stack implementation using python list
 class LStack:
-    def __init__(self):
+    def __init__(self):  # O(1)
         self.stk = []
 
-    def push(self, val):
+    def push(self, val):  # O(1)
         self.stk.append(val)
 
-    def pop(self):
+    def pop(self):  # O(1)
         if self.is_empty():
             return None
         return self.stk.pop()
 
-    def peek(self):
+    def peek(self):  # O(1)
         if self.is_empty():
             return None
         return self.stk[-1]
 
-    def clear(self):
+    def clear(self):  # O(1)
         self.stk = []
 
-    def is_empty(self):
+    def is_empty(self):  # O(1)
         return len(self.stk) == 0
 
-    def size(self):
+    def size(self):  # O(1)
         return len(self.stk)
--- ds/tree.py
+++ ds/tree.py
@@ -6,12 +6,12 @@ from __future__ import annotations
 
 
 class TreeNode:
-    def __init__(self, val):
+    def __init__(self, val):  # O(1)
         self.val = val
         self.parent = None
         self.children = []
 
-    def get_level(self):
+    def get_level(self):  # O(n)
         level = 0
         p = self.parent
         while p:
@@ -19,14 +19,14 @@ class TreeNode:
             p = p.parent
         return level
 
-    def add_child(self, child):
+    def add_child(self, child: TreeNode):  # O(1)
         child.parent = self
         self.children.append(child)
 
-    def remove_child(self, child):
+    def remove_child(self, child):  # O(n)
         self.children.remove(child)
 
-    def print_tree(self):
+    def print_tree(self):  # O(n)
         spaces = " " * self.get_level() * 3
         prefix = spaces + "|__" if self.parent else ""
 
