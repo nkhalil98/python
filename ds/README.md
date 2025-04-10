396b14935958d2b22d916ba2ff8ee0db7e4655df
nkhalil98 <nabilamirkhalil@proton.me>
2025-04-09 23:11:43 -0700

    lru cache implementation

--- ds/README.md
+++ ds/README.md
@@ -15,3 +15,4 @@
   - Min-Heap
   - Max-Heap
 - Graph
+- LRU Cache
---
+++ ds/lru.py
@@ -0,0 +1,55 @@
+"""
+Least Recently Used (LRU) Cache
+"""
+
+from __future__ import annotations
+
+
+class Node:
+    def __init__(self, key: int, value: int):
+        self.key = key
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class LRUCache:
+    def __init__(self, capacity: int) -> None:
+        self.capacity = capacity
+        self.cache = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node: Node) -> None:
+        prev = node.prev
+        next = node.next
+        prev.next = next
+        next.prev = prev
+
+    def _add(self, node: Node) -> None:
+        prev = self.tail.prev
+        prev.next = node
+        node.prev = prev
+        node.next = self.tail
+        self.tail.prev = node
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add(node)
+            return node.value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+        node = Node(key, value)
+        self._add(node)
+        self.cache[key] = node
+        if len(self.cache) > self.capacity:
+            lru_node = self.head.next
+            self._remove(lru_node)
+            del self.cache[lru_node.key]
