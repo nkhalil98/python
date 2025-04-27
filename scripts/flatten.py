5b7c6d307b03914c1ef232f1e8e4eb6211adfc22
nkhalil98 <nabilamirkhalil@proton.me>
2025-04-26 19:23:42 -0700

    cleanup recursion scripts

--- scripts/flatten.py
+++
@@ -1,9 +0,0 @@
-def flatten(my_list):
-    result = []
-    for el in my_list:
-        if isinstance(el, list):
-            flat_list = flatten(el)
-            result += flat_list
-        else:
-            result.append(el)
-    return result
