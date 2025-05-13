f278a8110504c68c097de1397fcc08eff22b0089
nkhalil98 <nabilamirkhalil@proton.me>
2025-05-13 16:11:05 -0700

    comments

--- scripts/tricks.py
+++ scripts/tricks.py
@@ -1,4 +1,4 @@
-# methods that mutate the list in place return None
+# methods that mutate lists in-place return None
 L1 = [2, 1]
 L2 = [3]
 L3 = L2.append(L1)  # L3 = None
@@ -14,25 +14,26 @@ L = [1, 2, 3, 4]
 for i in range(len(L)):
     L.append(i)
 
-## L is mutated each iteration
+## L is mutated in-place each iteration
 L = [1, 2, 3, 4]
-i = 0
 
-for e in L:  # infinite loop
+i = 0
+for e in L:  # infinite loop (keeps looping over the newly added elements)
     L.append(i)
     i += 1
 
 ## L is mutated each iteration but not in-place
 L = [1, 2, 3, 4]
-for e in L:  # iterate over the original list
-    L = L + L
-print(L)  # [1, 2, 3, 4, 1, 2, 3, 4, ...]
 
-## L is mutated each iteration
+# looping walks over the original object pointed to when called (original list)
+for e in L:
+    L = L + L
+
+## L is mutated in-place each iteration
 L = [1, 2, 2, 2]
-for e in L:  # iterate over the original list
+
+for e in L:  # pointer does not shift back when an element is removed
     L.remove(2)
-print(L)  # [1, 2]
 
 
 # use id to check objects in memory
