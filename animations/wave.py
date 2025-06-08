eaac7d8f3c4ac79368715c3cc4a11cc1c15e8c4a
nkhalil98 <nabilamirkhalil@proton.me>
2025-06-08 12:42:22 -0700

    add colors

--- animations/wave.py
+++ animations/wave.py
@@ -4,7 +4,17 @@ import sys
 
 def main():
     global num, increase
-    print("*" * num**2)
+    colors = [
+        "\033[31m",  # red
+        "\033[32m",  # green
+        "\033[33m",  # yellow
+        "\033[34m",  # blue
+        "\033[35m",  # purple
+        "\033[36m",  # cyan
+        "\033[37m",  # white
+    ]
+    color = colors[num % len(colors)]
+    print(f"{color}*" * num**2 + "\033[0m")
     if increase:
         num += 1
         if num == 9:
