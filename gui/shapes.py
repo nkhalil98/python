78df6562e14ef704955d9e4fdbedfef4b8bf8ad9
nkhalil98 <nabilamirkhalil@proton.me>
2025-09-15 21:20:20 -0700

    add koch snowflake; rename funcs

--- gui/shapes.py
+++ gui/shapes.py
@@ -1,6 +1,6 @@
 import math
 
-from turtle import Turtle, forward, left
+from turtle import Turtle, forward, left, right
 
 
 t = Turtle()
@@ -8,48 +8,61 @@ t.speed(0)
 t.hideturtle()
 
 
-def draw_polyline(n: int, length: int, angle: int) -> None:
+def polyline(n: int, length: int, angle: int) -> None:
     for _ in range(n):
         forward(length)
         left(angle)
 
 
-def draw_polygon(sides: int, size: int) -> None:
+def polygon(sides: int, size: int) -> None:
     angle = 360 / sides
-    draw_polyline(sides, size, angle)
+    polyline(sides, size, angle)
 
 
-def draw_square(size: int) -> None:
-    draw_polygon(4, size)
+def square(size: int) -> None:
+    polygon(4, size)
 
 
-def draw_parallelogram(base: int, height: int) -> None:
+def parallelogram(base: int, height: int, angle: int) -> None:
     for _ in range(2):
         forward(base)
-        left(60)
+        left(angle)
         forward(height)
-        left(120)
+        left(180 - angle)
 
 
-def draw_rectangle(length: int, width: int) -> None:
-    for _ in range(2):
-        forward(length)
-        left(90)
-        forward(width)
-        left(90)
+def rectangle(length: int, width: int) -> None:
+    parallelogram(length, width, 90)
 
 
-def draw_arc(radius: int, angle: int) -> None:
+def rhombus(size: int, angle: int) -> None:
+    parallelogram(size, size, angle)
+
+
+def arc(radius: int, angle: int) -> None:
     arc_length = 2 * math.pi * radius * abs(angle) / 360
     n = int(arc_length / 3) + 1
     step_length = arc_length / n
     step_angle = angle / n
-    draw_polyline(n, step_length, step_angle)
+    polyline(n, step_length, step_angle)
 
 
-def draw_circle(radius: int) -> None:
-    draw_arc(radius, 360)
+def circle(radius: int) -> None:
+    arc(radius, 360)
+
+
+def koch(length: int) -> None:
+    if length < 3:
+        forward(length)
+        return
+    koch(length / 3)
+    left(60)
+    koch(length / 3)
+    right(120)
+    koch(length / 3)
+    left(60)
+    koch(length / 3)
 
 
 if __name__ == "__main__":
-    draw_circle(120)
+    koch(120)
